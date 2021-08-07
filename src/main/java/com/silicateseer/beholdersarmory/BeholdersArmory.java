201dbb9a9003f6448ff5062dc0695087f0fdcc9f
Wondering-Oracle <73407726+Wondering-Oracle@users.noreply.github.com>
2021-08-07 11:53:41 -0400

    wtv

--- src/main/java/com/silicateseer/beholdersarmory/BeholdersArmory.java
+++ src/main/java/com/silicateseer/beholdersarmory/BeholdersArmory.java
@@ -17,6 +17,14 @@ public class BeholdersArmory implements ModInitializer {
 	public static final Item STEEL_INGOT = register("steel_ingot", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 	
 	public static final Item SILVER_HELMET = register("silver_helmet", new ArmorItem(BAArmorMaterials.SILVER, EquipmentSlot.HEAD,new FabricItemSettings().group(ItemGroup.COMBAT)));
+	public static final Item SILVER_CHESTPLATE = register("silver_chestplate", new ArmorItem(BAArmorMaterials.SILVER, EquipmentSlot.CHEST,new FabricItemSettings().group(ItemGroup.COMBAT)));
+	public static final Item SILVER_LEGGINGS = register("silver_leggings", new ArmorItem(BAArmorMaterials.SILVER, EquipmentSlot.LEGS,new FabricItemSettings().group(ItemGroup.COMBAT)));
+	public static final Item SILVER_BOOTS = register("silver_boots", new ArmorItem(BAArmorMaterials.SILVER, EquipmentSlot.FEET,new FabricItemSettings().group(ItemGroup.COMBAT)));
+
+	public static final Item EMERALD_HELMET = register("emerald_helmet", new ArmorItem(BAArmorMaterials.EMERALD, EquipmentSlot.HEAD,new FabricItemSettings().group(ItemGroup.COMBAT)));
+	public static final Item EMERALD_CHESTPLATE = register("emerald_chestplate", new ArmorItem(BAArmorMaterials.EMERALD, EquipmentSlot.CHEST,new FabricItemSettings().group(ItemGroup.COMBAT)));
+	public static final Item EMERALD_LEGGINGS = register("emerald_leggings", new ArmorItem(BAArmorMaterials.EMERALD, EquipmentSlot.LEGS,new FabricItemSettings().group(ItemGroup.COMBAT)));
+	public static final Item EMERALD_BOOTS = register("emerald_boots", new ArmorItem(BAArmorMaterials.EMERALD, EquipmentSlot.FEET,new FabricItemSettings().group(ItemGroup.COMBAT)));
 
 	
 	@Override
